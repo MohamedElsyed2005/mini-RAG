260cec6d0bafdf9c8fc85deded2ead4c4295169e
mohamed <2305273@anu.edu.eg>
2026-09-20 10:47:15 +0300

    feat: add random string for file nams and make the code more solid

--- src/routes/data.py
+++ src/routes/data.py
@@ -12,7 +12,9 @@ from helpers.config import get_settings, Settings
 from controllers import DataController, ProjectController
 import aiofiles
 from models import ResponseSignal
+import logging 
 
+logger = logging.getLogger("uvicorn.error")
 # Create the versioned API router.
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -23,7 +25,8 @@ data_router = APIRouter(
 async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
     # validate the file properties
-    is_valid, result_signal = DataController().validate_uploaded_file(file = file)
+    data_controller = DataController()
+    is_valid, result_signal = data_controller.validate_uploaded_file(file = file)
     if not is_valid:
         return JSONResponse(
                     status_code = status.HTTP_400_BAD_REQUEST, 
@@ -31,13 +34,23 @@ async def upload_data(project_id: str, file: UploadFile,
                     "signal": result_signal
                         })
     project_dir_path = ProjectController().get_project_path(project_id = project_id)
-    file_path = os.path.join(project_dir_path, file.filename)
+    file_path = data_controller.generate_unique_filename(orig_file_name = file.filename, project_id = project_id)
 
-    # Reset pointer before reading
-    await file.seek(0)
-    async with aiofiles.open(file_path, "wb") as f:
-        while chunk:= await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
+    try:
+        # Reset pointer before reading
+        await file.seek(0)
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk:= await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        logger.error(f"Erorr while uploading file: {e}")
+        
+        return JSONResponse(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            content = {
+                "signal", ResponseSignal.FILE_UPLOAD_FAILED.value
+            }
+        )
     return JSONResponse(
     status_code = status.HTTP_200_OK, 
     content = {
